eb2dc407218aef853d7616be4185dcc8d3fd87d4
psixonaut <podmax25@gmail.com>
2025-03-18 23:44:35 +0300

    Сделал коллизию
    Вроде сделал коллизию с жёлтыми тайлами, но надо проверить

--- scr/Game/game.gd
+++ scr/Game/game.gd
@@ -5,18 +5,23 @@ var tile_size: int =  16
 
 var tilemap: TileMap
 var paths: Array = []
+var map_x: int = 60
+var map_y: int = 30
 
 func _ready():
 	tilemap = get_node("TileMap")
-	root_node  = map_generator.new(Vector2i(0, 0), Vector2i(60, 30)) #устанавливаем размер карты
+	root_node  = map_generator.new(Vector2i(0, 0), Vector2i(map_x, map_y)) #устанавливаем размер карты
 	root_node.split(2, paths) #кол-во комнат = 2 в степени первого числа
 	queue_redraw()
 	pass 
 
 func is_inside_padding(x, y, leaf, padding): #проверка на границу комнаты
-	return x <= padding.x or y <= padding.y or x >= leaf.size.x - padding.z or y >= leaf.size.y - padding.w
+	return x <= padding.x or y <= padding.y or x >= leaf.size.x - padding.z or y >= leaf.size.y - padding.w 
 
 func _draw():
+	for x in map_x:
+			for y in map_y:
+				tilemap.set_cell(0, Vector2i(x, y), 0, Vector2i(5,7))
 	var rng = RandomNumberGenerator.new()
 	for leaf in root_node.get_leaves():
 		var padding = Vector4i(1, 1, 1, 1)
