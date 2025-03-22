ddc2f7345d80079a9efa6fc1b2a43ba5041f2fcb
NikTak777 <nikkondrakhin@gmail.com>
2025-03-22 16:25:44 +0300

    Обновление механики перемещения персонажа
    Управление было изменено на клавиши WASD. Также в скрипт ZoomController была добавлена функция изменения зума камеры по нажатию кнопки Q.

--- scr/Entities/Player/dice.gd
+++ scr/Entities/Player/dice.gd
@@ -6,7 +6,7 @@ func _ready():
 	position = Vector2.ZERO # Устанавливает начальную позицию на (0, 0)
 
 func get_movement_direction():
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	return input_direction
 	
 
--- scr/Utils/ZoomController/zoom_controller.gd
+++ scr/Utils/ZoomController/zoom_controller.gd
@@ -7,6 +7,8 @@ extends Node
 @export var transition_type: Tween.TransitionType = Tween.TRANS_LINEAR
 @export var ease_type: Tween.EaseType = Tween.EASE_IN_OUT
 
+var flag_zoom = false
+
 func _ready():
 	if camera:
 		var tween = create_tween()
@@ -16,3 +18,16 @@ func _ready():
 		tween.tween_property(camera, "zoom", 
 			Vector2(target_zoom, target_zoom), duration
 		).from(Vector2(start_zoom, start_zoom))
+		flag_zoom = true
+		
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("change_zoom"):
+		change_zoom()
+		
+func change_zoom():
+	if flag_zoom:
+		camera.zoom = Vector2(1.0, 1.0)
+	else:
+		camera.zoom = Vector2(7.0, 7.0)
+	flag_zoom = !flag_zoom
+		
