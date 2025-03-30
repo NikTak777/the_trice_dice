c5911b4d1a8771f823dcffb20227f7ec6081f857
NikTak777 <nikkondrakhin@gmail.com>
2025-03-31 01:51:36 +0300

    Добавление меню в основу игры
    Я объединил код с ветки main_menu и dev. Теперь в игре появилось главное меню. При нажатии на ESC игра становится на паузу и на экране появляется три кнопки: Restart, Settings и Exit. Первая кнопка перезапускает игру, вторая - пустышка (при ненадобности удалим) и третья - закрывает окно с игрой. Также из паузы можно выйти повторным нажатием на ESC, и игра возобновится.

--- scr/Entities/Player/dice.gd
+++ scr/Entities/Player/dice.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var menu = $"../CanvasLayer/MainMenu" # Подключаем меню
+
 const SPEED = 100.0
 
 func _ready():
@@ -9,10 +11,12 @@ func get_movement_direction():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	return input_direction
 	
-
 func _process(delta):
-	var diretion = get_movement_direction()
-	
-	velocity = diretion * SPEED
-	
-	move_and_slide()
+	if !get_tree().paused:
+		var direction = get_movement_direction()
+		velocity = direction * SPEED
+		move_and_slide()
+
+func toggle_pause() -> void:
+	get_tree().paused = !get_tree().paused
+	menu.visible = !menu.visible
---
+++ scr/UserInterface/MainMenu/main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+func _ready() -> void:
+	visible = false  # Меню скрыто по умолчанию
+
+func _process(_delta):
+	if Input.is_action_just_pressed("pause"):
+		toggle_menu()
+
+func toggle_menu():
+	visible = !visible
+	get_tree().paused = visible  # Ставим или снимаем паузу
+
+func _on_start_pressed() -> void:
+	get_tree().paused = false  # Убираем паузу
+	visible = false  # Скрываем меню
+	get_tree().reload_current_scene()  # Перезапускаем игру
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()  # Закрываем игру
