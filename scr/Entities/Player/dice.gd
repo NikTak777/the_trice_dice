4baa68cba35bb7db22731d08d010850d6f623799
NikTak777 <nikkondrakhin@gmail.com>
2025-03-31 14:48:14 +0300

    Добавил шкалу здоровья
    У главного героя появились скрипт получения урона, система здоровья и её шкала. Теперь персонаж может получать урон (пока при нажатии на кнопку E), его здоровье будет уменьшаться, а при достижении значения 0, персонаж погибнет и исчезнет из игрового мира. Также шкала здоровья меняет свой цвет в зависимости от значения здоровья. Изначально персонаж обладает 100 единицами здоровья, и шкала окрашена в зелёный цвет. При достижении отметки в 50 единиц шкала меняет свой цвет на жёлтый. Аналогично она перекрашивается в красный при уровне здоровья в 20 единиц. При перезапуске игры через игровое меню здоровье персонажа возвращается на начальное значение.

--- scr/Entities/Player/dice.gd
+++ scr/Entities/Player/dice.gd
@@ -2,10 +2,17 @@ extends CharacterBody2D
 
 @onready var menu = $"../CanvasLayer/MainMenu" # Подключаем меню
 
+const HEALTHBAR_SCENE = preload("res://scr/UserInterface/HealthBar/HealthBar.tscn")  # Загружаем сцену заранее
+
 const SPEED = 100.0
+
+var max_hp = 100
+var current_hp = 100
+var hp_bar = null  # Здесь будем хранить ссылку на HealthBar
 
 func _ready():
 	position = Vector2.ZERO # Устанавливает начальную позицию на (0, 0)
+	spawn_health_bar()
 
 func get_movement_direction():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -16,7 +23,27 @@ func _process(delta):
 		var direction = get_movement_direction()
 		velocity = direction * SPEED
 		move_and_slide()
+	if Input.is_action_just_pressed("damage"):
+		print("Take damage!")
+		take_damage(10)
 
 func toggle_pause() -> void:
 	get_tree().paused = !get_tree().paused
 	menu.visible = !menu.visible
+	
+
+func spawn_health_bar():
+	hp_bar = HEALTHBAR_SCENE.instantiate()  # Создаём экземпляр HealthBar
+	add_child(hp_bar)  # Добавляем его как дочерний узел
+	hp_bar.position = Vector2(-110, 100)  # Смещаем немного вверх
+
+func take_damage(amount: int):
+	current_hp -= amount
+	current_hp = max(current_hp, 0)
+	hp_bar.set_hp(current_hp)  # Обновляем шкалу HP
+
+	if current_hp == 0:
+		die()
+
+func die():
+	queue_free()  # Удаляем персонажа
