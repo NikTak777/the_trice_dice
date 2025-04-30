7e92121c66718d35c3befc78805f5d7cf6f48cee
NikTak777 <nikkondrakhin@gmail.com>
2025-04-30 14:16:05 +0300

    Добавил движение врагам
    Враги теперь могут двигаться в сторону главного персонажа, появилась возможность выиграть при убийстве всех врагов. А также были исправлены ошибки распознавания главного персонажа в комнатах

---
+++ Utils/WeaponSpawner/weapon_spawner.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var weapon_scene = preload("res://scr/Objects/Weapon/weapon.tscn")  # Загружаем сцену оружия
+
+var factory_scene = preload("res://scr/Utils/WeaponFactory/WeaponFactory.tscn")
+
+# Спавн оружия во второй комнате с заданным типом
+func spawn_weapon_in_room(room_number: int, map_generator, weapon_type: String) -> Node:
+	var weapon_factory = factory_scene.instantiate()
+	var weapon = weapon_factory.create_weapon(weapon_type)
+	add_child(weapon)
+	
+	var spawn_position = map_generator.get_room_center(room_number) * 16
+	weapon.position = spawn_position
+	
+	print("Оружие", weapon.weapon_name, "заспавнено в позиции:", weapon.position)
+	return weapon
