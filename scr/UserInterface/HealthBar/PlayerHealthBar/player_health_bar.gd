15636aca4c6331ac735e030a951bd5cd72956484
NikTak777 <nikkondrakhin@gmail.com>
2025-07-24 15:02:12 +0300

    Обновление шкалы здоровья главного персонажа
    Шкала здоровья будет способна изменять свою длину на экране в зависимости от значения максимального количества здоровья.

--- scr/UserInterface/HealthBar/PlayerHealthBar/player_health_bar.gd
+++ scr/UserInterface/HealthBar/PlayerHealthBar/player_health_bar.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var white_bar = $WhiteBar
 var tween: Tween
 
+var base_width: float = 350.0
+
 func _ready():
 	tween = create_tween()
 	tween.set_loops(0)  # Один раз
@@ -11,6 +13,15 @@ func _ready():
 func set_max_hp(value: int):
 	green_bar.max_value = value
 	white_bar.max_value = value
+	
+	var target_width = base_width * (value / 100.0) # Вычисляем новую ширину на основе максимального HP
+	
+	# Анимируем ширину обоих баров
+	var size_tween = create_tween()
+	size_tween.tween_property(green_bar, "custom_minimum_size", Vector2(target_width, green_bar.size.y), 0.5)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	size_tween.tween_property(white_bar, "custom_minimum_size", Vector2(target_width, white_bar.size.y), 0.5)\
+		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 
 func set_hp(value: int):
 	value = clamp(value, 0, green_bar.max_value)
