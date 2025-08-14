a5feb9508de408f639e422f66b776f768b01862a
NikTak777 <nikkondrakhin@gmail.com>
2025-08-14 21:44:11 +0300

    Добавлен менеджер настроек
    Написан автозагружаемый скрипт для управления настройками через меню настроек.

---
+++ scr/Utils/Autoload/SettingsManager/SettingsManager.gd
@@ -0,0 +1,42 @@
+extends Node
+
+var master_volume: float = 1.0 # Уровень громкости
+var fullscreen: bool = false # Статус полнокэранного режима
+var difficulty: String = "normal" # Режим сложности по умолчанию
+
+var difficulties = ["easy", "normal", "hard"] # Список режимов сложности
+var current_difficulty: int = 1 # индекс текущей сложности в массиве difficulties
+
+# Настройки разброса и скорострельность для каждой сложности
+var boss_inaccuracy_settings = {
+	"easy": {"min_ang": 7.0, "max_ang": 10.0, "interval": 0.7},
+	"normal": {"min_ang": 3.0, "max_ang": 8.0, "interval": 0.5},
+	"hard": {"min_ang": 0.0, "max_ang": 0.0, "interval": 0.3}
+}
+
+func apply_settings():
+	# Громкость
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(master_volume))
+	# Полноэкранный режим
+	if fullscreen:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+# Смена сложности влево (уменьшение)
+func decrease_difficulty():
+	current_difficulty = (current_difficulty - 1 + difficulties.size()) % difficulties.size()
+	difficulty = difficulties[current_difficulty]
+
+# Смена сложности вправо (увеличение)
+func increase_difficulty():
+	current_difficulty = (current_difficulty + 1) % difficulties.size()
+	difficulty = difficulties[current_difficulty]
+
+# Получение текущей сложности
+func get_current_difficulty() -> String:
+	return difficulties[current_difficulty]
+	
+# Получение текущего значения разброса босса
+func get_boss_inaccuracy() -> Dictionary:
+	return boss_inaccuracy_settings[difficulty]
