65e6eca3e9a391f31d75897dec3f6db4a30da55f
NikTak777 <nikkondrakhin@gmail.com>
2025-08-15 19:29:49 +0300

    Обновление главного меню
    В главном меню появилось поле вывода статистики предыдущей выигрышной игры (режим сложности и потраченное время)

--- scr/UserInterface/MainMenu/main_menu.gd
+++ scr/UserInterface/MainMenu/main_menu.gd
@@ -1,10 +1,20 @@
 extends Control
 
+@onready var last_game_info: Label = $LastGameInfoContainer/LastGameInfoText
+
 func _ready():
 	# Подключение сигналов
 	$VBoxContainer/Button.pressed.connect(_on_start_button_pressed)
 	$VBoxContainer/Button2.pressed.connect(_on_settings_button_pressed)
 	$VBoxContainer/Button3.pressed.connect(_on_exit_button_pressed)
+	
+	if not Global.is_last_game_victory:
+		last_game_info.text = ""
+	else:
+		last_game_info.text = "Last game info:\nDifficulty: %s\nTime: %.2f" % [
+			Global.last_game_difficulty,
+			Global.last_run_time
+		]
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scr/Game/game.tscn")
@@ -16,3 +26,4 @@ func _on_settings_button_pressed():
 
 func _on_exit_button_pressed():
 	get_tree().quit()
+	
