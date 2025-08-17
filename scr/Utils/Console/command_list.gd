720fede888ed2296d972615dda58ce411ddcf26e
NikTak777 <nikkondrakhin@gmail.com>
2025-08-18 00:18:11 +0300

    Исправлен баг неработающей консоли в exe сборках
    Добавлен отдельный скрипт command_list с полным списком ссылок на команды консоли разработчика. Это позволяет работать консоли в exe сборках.

---
+++ scr/Utils/Console/command_list.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# Список всех команд (пути к .gd)
+const COMMANDS = [
+	"res://scr/Utils/Console/Commands/cmd_ability.gd",
+	"res://scr/Utils/Console/Commands/cmd_clear.gd",
+	"res://scr/Utils/Console/Commands/cmd_default.gd",
+	"res://scr/Utils/Console/Commands/cmd_give.gd",
+	"res://scr/Utils/Console/Commands/cmd_health.gd",
+	"res://scr/Utils/Console/Commands/cmd_help.gd",
+	"res://scr/Utils/Console/Commands/cmd_kill.gd",
+	"res://scr/Utils/Console/Commands/cmd_restart.gd",
+	"res://scr/Utils/Console/Commands/cmd_show_nodes.gd",
+	"res://scr/Utils/Console/Commands/cmd_zoom.gd",
+]
